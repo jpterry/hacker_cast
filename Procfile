85d7b1344ab0eca8cd5a06e490bdda3ce09a37d2
John Terry <jpterry@gmail.com>
2013-02-05 00:22:44 -0800

    Refactored and cleaned up WebsocketServer
    Renamed to lib/signaller

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec rails server
-websocket: bundle exec ruby ./lib/websocket_server.rb
+websocket: bundle exec ruby ./lib/signaler.rb
---
+++ lib/signaler.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+
+require 'eventmachine'
+require 'em-websocket'
+require 'json'
+
+require 'securerandom'
+
+module SignalDispatch
+  class Client
+    attr_reader :websocket, :pair
+    def initialize(websocket)
+      @websocket = websocket
+      @queue = EM::Queue.new
+    end
+
+    def handle_message(msg)
+      @queue.push(msg)
+    end
+
+    def set_pair(other)
+      @pair = other
+      unless other.pair
+        other.set_pair(self)
+      end
+      @queue.pop do |msg|
+        @pair.websocket.send(msg)
+      end
+      @pair
+    end
+  end
+
+  class Signaller
+    #singleton
+    def self.instance
+      @_instance ||= new
+    end
+
+    def self.start!
+      EM.run do
+        EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
+          ws.onopen { |handshake| Signaller.instance.add_client(ws) }
+          ws.onclose { Signaller.instance.remove_client(ws) }
+          ws.onmessage { |msg| Signaller.instance.handle_message(ws) }
+        end
+      end
+    end
+
+    def initialize
+      @clients = {}
+    end
+
+    def add_client(ws)
+      @clients[ws] = Client.new(ws)
+      pair_if_possible(@clients[ws])
+    end
+
+    def remove_client(ws)
+      @clients.delete(ws)
+    end
+
+    def handle_message(ws, msg)
+      @clients[ws].handle_message(msg)
+    end
+
+    # PAIR EM UP
+    def pair_if_possible(client)
+      mate = @clients.reject { |k, v| v == client }.find { |k, v| v.pair.nil? }
+      client.set_pair(mate[1]) if mate
+    end
+  end
+end
+
+SignalDispatch::Signaller.start!
