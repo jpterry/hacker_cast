2415d86e90e295ec3ed5d2deb7dbd288c4d1fcf6
John Terry <jpterry@gmail.com>
2013-02-24 15:21:39 -0800

    Update deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,11 +26,42 @@ set :default_environment, {
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :foreman do
+  desc "Export the Procfile to Ubuntu's upstart scripts"
+  task :export, :roles => :app do
+    run "cd #{current_path} && sudo bundle exec foreman export upstart /etc/init " +
+        "-f ./Procfile.prod -a #{application} -u nobody -l #{shared_path}/log"
+  end
+
+  desc "Start the application services"
+  task :start, :roles => :app do
+    sudo "start #{application}"
+  end
+
+  desc "Stop the application services"
+  task :stop, :roles => :app do
+    sudo "stop #{application}"
+  end
+
+  desc "Restart the application services"
+  task :restart, :roles => :app do
+    run "sudo start #{application} || sudo restart #{application}"
+  end
+
+  desc "Display logs for a certain process - arg example: PROCESS=web-1"
+  task :logs, :roles => :app do
+    run "cd #{current_path}/log && cat #{ENV["PROCESS"]}.log"
+  end
+end
+
+namespace :deploy do
+  task :start do
+    run "cd #{current_path}; bundle exec foreman -f Procfile.prod start"
+  end
+  task :stop do
+    run "cd #{current_path}; bundle exec foreman -f Procfile.prod stop"
+  end
+  task :restart do
+    #run "cd #{current_path}; bundle exec foreman -f Procfile.prod restart"
+  end
+end
